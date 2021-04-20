036725921cf76ec2b85bd7f287edfd172f706d2a
Flor Pistán Esparza <flor.pistan.esparza@gmail.com>
2021-04-19 22:55:56 -0300

    deleting files - restructuring app

--- src/App.js
+++ src/App.js
@@ -1,22 +1,9 @@
 import React from 'react'
-import TopBar from './components/TopBar'
-import Header from './components/Header'
-import Curriculum from './components/Curriculum'
-import Wave from './components/Wave'
-import Proyectos from './components/Proyectos.jsx'
-import WaveTwo from './components/WaveTwo.jsx'
-import LinkedInProfile from './components/LinkedInProfile.jsx'
 
 function App() {
   return (
     <div className="App">
-      <TopBar />
-      <Header />
-      <Curriculum />
-      <Wave />
-      <Proyectos />
-      <WaveTwo />
-      <LinkedInProfile />
+
     </div>
   );
 }
